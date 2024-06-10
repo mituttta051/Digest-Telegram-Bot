520d397e4758aaeb016db37137bc5fd503b0685e
spiriton <vanya.ershov.05@mail.ru>
2024-06-11 02:05:02 +0300

    update

--- handlers/generate.py
+++ handlers/generate.py
@@ -1,8 +1,6 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-
-from config import CHANNEL_ID
 from utils.botUtils import getMessagesLastWeek
 
 generate_router = Router()
@@ -10,4 +8,4 @@ generate_router = Router()
 
 @generate_router.message(Command('generate'))
 async def get_posts(message: Message):
-    await message.reply(str(getMessagesLastWeek(CHANNEL_ID)))
+    await message.reply(str(getMessagesLastWeek(message.text.lstrip("/generate ").lstrip())))
