1a0ac6ae00beb4c6dc822845e735c87c4e1ffa8e
ClayMix <claymix007@gmail.com>
2024-06-15 14:24:52 +0300

    Added button and keyboard for settings branch

--- content/keyboards/settings_keyboards.py
+++ content/keyboards/settings_keyboards.py
@@ -1,1 +1,9 @@
 # A file that will store settings branch keyboards
+# Import downloaded packages
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+settings_back_inline_button = InlineKeyboardButton(text="Return back", callback_data="settings_back")
+
+settings_inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [settings_back_inline_button]
+])
