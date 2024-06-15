fa71b6912bd51b8270249f715c8c9055574b0ea9
ClayMix <claymix007@gmail.com>
2024-06-15 14:25:50 +0300

    Added more buttons to settings branch. Preparing for future

--- content/keyboards/settings_keyboards.py
+++ content/keyboards/settings_keyboards.py
@@ -3,7 +3,22 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 settings_back_inline_button = InlineKeyboardButton(text="Return back", callback_data="settings_back")
+change_bot_language_inline_button = InlineKeyboardButton(text="Change bot language",
+                                                         callback_data="change_bot_language")
+change_api_key_inline_button = InlineKeyboardButton(text="Change LLM API key", callback_data="change_llm_api_key")
 
 settings_inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [settings_back_inline_button]
+    [settings_back_inline_button],
+    [change_bot_language_inline_button],
+    [change_api_key_inline_button]
 ])
+
+
+def get_supported_bot_languages():
+    # Get some data from database
+    pass
+
+
+def get_supported_llms():
+    # Get some data from database
+    pass
